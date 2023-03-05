be8bd5dc4bc65766fb0c788040a6a5408a927e2a
netsyak <netsyak@naver.com>
2023-03-05 18:54:30 +0900

    스프링부트-연관 관계 매핑(1:1 단방향 매핑)

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -1,8 +1,14 @@
 package com.shop.entity;
 
+import com.shop.dto.MemberFormDto;
+import com.shop.repository.CartRepository;
+import com.shop.repository.MemberRepository;
+import groovy.transform.ASTTest;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
@@ -20,5 +26,12 @@ public class Cart {
     @OneToOne
     @JoinColumn(name="member_id")
     private Member member;
+
+//    @OneToOne(Fetch = FetchType.EAGER) // 즉시로딩 으로 설정하기
+//    @JoinColumn(name="member_id")
+//    private Member member;
+//
+
+
 
 }
