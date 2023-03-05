fccfdd96c132df897bc66d3059550ac86421d66d
netsyak <netsyak@naver.com>
2023-03-05 11:26:21 +0900

    스프링부트

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -36,7 +36,7 @@ public class MemberController {
     public String loginMember(){
         return "/member/memberLoginForm";
     }
-    @GetMapping(value = "/login/erroe")
+    @GetMapping(value = "/login/error")
     public String loginError(Model model){
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
         return "/member/memberLoginForm";
