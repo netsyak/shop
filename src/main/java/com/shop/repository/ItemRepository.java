422719c5fa01d950d0bb0f00da04f0747d11582c
netsyak <netsyak@naver.com>
2023-03-08 09:39:27 +0900

    스프링부트-상품등록 및 조회 하기

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>,
-        QuerydslPredicateExecutor<Item> {
+        QuerydslPredicateExecutor<Item>,ItemRepositoryCustom {
 
     List<Item> findByItemNm(String itemNm); //상품 이름 찾기
 
